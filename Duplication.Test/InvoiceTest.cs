81bce0095de5e04a7281f13041825d22a1bb55dd
Jason Gorman <jason.gorman@codemanship.com>
2025-12-07 10:01:56 +0000

    Added projects for Duplication

---
+++ Duplication.Test/InvoiceTest.cs
@@ -0,0 +1,68 @@
+﻿namespace Duplication.Test;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class InvoiceTests
+{
+    [Test]
+    public void CalculateTotal_AllItemsPresent_ReturnsCorrectTotal()
+    {
+        var item1 = new Item(10.0, 2); // 20
+        var item2 = new Item(5.0, 3);  // 15
+        var item3 = new Item(7.5, 1);  // 7.5
+        var item4 = new Item(2.0, 5);  // 10
+        var item5 = new Item(12.0, 1); // 12
+
+        var invoice = new Invoice(item1, item2, item3, item4, item5);
+
+        double expectedTotal = 20 + 15 + 7.5 + 10 + 12;
+
+        Assert.That(invoice.CalculateTotal(), Is.EqualTo(expectedTotal));
+    }
+
+    [Test]
+    public void CalculateTotal_SomeItemsNull_IgnoresNullItems()
+    {
+        var item1 = new Item(10.0, 2); // 20
+        Item item2 = null;
+        var item3 = new Item(7.5, 1);  // 7.5
+        Item item4 = null;
+        var item5 = new Item(12.0, 1); // 12
+
+        var invoice = new Invoice(item1, item2, item3, item4, item5);
+
+        double expectedTotal = 20 + 7.5 + 12;
+
+        Assert.That(invoice.CalculateTotal(), Is.EqualTo(expectedTotal));
+    }
+
+    [Test]
+    public void CalculateTotal_AllItemsNull_ReturnsZero()
+    {
+        var invoice = new Invoice(null, null, null, null, null);
+
+        Assert.That(invoice.CalculateTotal(), Is.EqualTo(0.0));
+    }
+
+    [Test]
+    public void CalculateTotal_SingleItemOnly_ReturnsSingleSubtotal()
+    {
+        var item1 = new Item(15.0, 3); // 45
+        var invoice = new Invoice(item1, null, null, null, null);
+
+        Assert.That(invoice.CalculateTotal(), Is.EqualTo(45.0));
+    }
+
+    [Test]
+    public void CalculateTotal_ZeroQuantityItems_ReturnsZeroForThoseItems()
+    {
+        var item1 = new Item(10.0, 0); // 0
+        var item2 = new Item(5.0, 2);  // 10
+        var invoice = new Invoice(item1, item2, null, null, null);
+
+        double expectedTotal = 0 + 10;
+
+        Assert.That(invoice.CalculateTotal(), Is.EqualTo(expectedTotal));
+    }
+}
---
+++ Duplication/Invoice.cs
@@ -0,0 +1,68 @@
+﻿namespace Duplication;
+
+public class Item
+{
+    public double Price { get; }
+    public int Quantity { get; }
+
+    public Item(double price, int quantity)
+    {
+        Price = price;
+        Quantity = quantity;
+    }
+}
+
+public class Invoice
+{
+    private readonly Item? _item1;
+    private readonly Item? _item2;
+    private readonly Item? _item3;
+    private readonly Item? _item4;
+    private readonly Item? _item5;
+
+    public Invoice(Item? item1, Item? item2, Item? item3, Item? item4, Item? item5)
+    {
+        _item1 = item1;
+        _item2 = item2;
+        _item3 = item3;
+        _item4 = item4;
+        _item5 = item5;
+    }
+
+    public double CalculateTotal()
+    {
+        double total = 0.0;
+
+        if (_item1 != null)
+        {
+            double subtotal1 = _item1.Price * _item1.Quantity;
+            total += subtotal1;
+        }
+
+        if (_item2 != null)
+        {
+            double subtotal2 = _item2.Price * _item2.Quantity;
+            total += subtotal2;
+        }
+
+        if (_item3 != null)
+        {
+            double subtotal3 = _item3.Price * _item3.Quantity;
+            total += subtotal3;
+        }
+
+        if (_item4 != null)
+        {
+            double subtotal4 = _item4.Price * _item4.Quantity;
+            total += subtotal4;
+        }
+
+        if (_item5 != null)
+        {
+            double subtotal5 = _item5.Price * _item5.Quantity;
+            total += subtotal5;
+        }
+
+        return total;
+    }
+}
