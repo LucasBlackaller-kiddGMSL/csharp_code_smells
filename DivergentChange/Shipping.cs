dbf961bbdc5d0b5a0ddd804cb8c0b3ef85a41069
Lucas BK <lucas.blackaller-kidd@gmsl.co.uk>
2025-12-10 16:35:23 +0000

    inline shipping

--- DivergentChange/Shipping.cs
+++ DivergentChange/Shipping.cs
@@ -4,15 +4,11 @@ public static class Shipping
 {
     public static decimal CalculateShipping(string region, decimal total)
     {
-        decimal shipping;
-
-        if (region == "EU")
-            shipping = total > 100 ? 0 : 10;
-        else if (region == "UK")
-            shipping = total > 50 ? 0 : 8;
-        else
-            shipping = total > 75 ? 0 : 15;
-
-        return shipping;
+        return region switch
+        {
+            "EU" => total > 100 ? 0 : 10,
+            "UK" => total > 50 ? 0 : 8,
+            _ => total > 75 ? 0 : 15
+        };
     }
 }
