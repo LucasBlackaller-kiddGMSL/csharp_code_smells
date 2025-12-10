d29c98f1794fa7f697ae48334702c489b694eaa1
Lucas BK <lucas.blackaller-kidd@gmsl.co.uk>
2025-12-10 16:38:18 +0000

    extract get tax rate

--- DivergentChange/Tax.cs
+++ DivergentChange/Tax.cs
@@ -4,14 +4,17 @@ public static class Tax
 {
     public static decimal CalculateTax(string jurisdiction, decimal total)
     {
-        var taxRate = jurisdiction switch
+        return total * GetTaxRate(jurisdiction);
+    }
+
+    private static decimal GetTaxRate(string jurisdiction)
+    {
+        return jurisdiction switch
         {
             "EU" => 0.20m,
             "UK" => 0.21m,
             "US" => 0.07m,
             _ => 0.10m
         };
-
-        return total * taxRate;
     }
 }
