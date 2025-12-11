dffd22598dd8da523a161d953ed0fd47f0f8f461
Charlie Curedale-Rayner <charlie.curedale-rayner@gmsl.co.uk>
2025-12-11 12:21:02 +0000

    move method to basket

--- FeatureEnvy/Model/Basket.cs
+++ FeatureEnvy/Model/Basket.cs
@@ -3,4 +3,22 @@
 public class Basket
 {
     public List<Item> Items { get; } = new();
+
+    public void AddProducts(Product product, int qty)
+    {
+        var existing = Items.FirstOrDefault(i => i.Product == product);
+
+        if (existing == null)
+        {
+            Items.Add(new Item
+            {
+                Product = product,
+                Quantity = qty
+            });
+        }
+        else
+        {
+            existing.Quantity += qty;
+        }
+    }
 }
--- FeatureEnvy/Services/BasketService.cs
+++ FeatureEnvy/Services/BasketService.cs
@@ -6,19 +6,6 @@ public class BasketService
 {
     public void AddToBasket(Basket basket, Product product, int qty)
     {
-        var existing = basket.Items.FirstOrDefault(i => i.Product == product);
-
-        if (existing == null)
-        {
-            basket.Items.Add(new Item
-            {
-                Product = product,
-                Quantity = qty
-            });
-        }
-        else
-        {
-            existing.Quantity += qty;
-        }
+        basket.AddProducts(product, qty);
     }
 }
