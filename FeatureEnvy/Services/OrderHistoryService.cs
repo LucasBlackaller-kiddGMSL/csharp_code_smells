09c1e2be8628d99b565e563a1f07f44b241834ac
Charlie Curedale-Rayner <charlie.curedale-rayner@gmsl.co.uk>
2025-12-11 12:33:16 +0000

    use order history

--- FeatureEnvy/Services/OrderHistoryService.cs
+++ FeatureEnvy/Services/OrderHistoryService.cs
@@ -14,24 +14,26 @@ public class OrderHistory
 
 public class OrderHistoryService
 {
+    private readonly OrderHistory _orderHistory;
     private readonly List<Order> _orders;
 
     public OrderHistoryService(OrderHistory orderHistory)
     {
+        _orderHistory = orderHistory;
         var orders = orderHistory.Orders;
         _orders = orders;
     }
 
     public IEnumerable<Order> FindOrdersByProduct(Product product)
     {
-        return _orders.Where(o =>
+        return _orderHistory.Orders.Where(o =>
             o.Confirmed &&
             o.Items.Any(i => i.Product == product));
     }
 
     public IEnumerable<Order> FindOrdersByAddress(Address address)
     {
-        return _orders.Where(o =>
+        return _orderHistory.Orders.Where(o =>
             o.Confirmed &&
             o.ShippingAddress.Country == address.Country);
     }
