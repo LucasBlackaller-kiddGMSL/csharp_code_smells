6021ba2128eaf6c59749b660791f2f8196be481b
Lucas BK <lucas.blackaller-kidd@gmsl.co.uk>
2025-12-11 16:49:34 +0000

    add constrcutor

--- Round2_tee/src/FootballScoreStats.cs
+++ Round2_tee/src/FootballScoreStats.cs
@@ -2,10 +2,17 @@
 
 public class FootballScoreStats
 {
+    private FootballData _footballData;
+
+    public FootballScoreStats()
+    {
+        _footballData = new FootballData();
+    }
+
     public int TeamTotal(string teamName)
     {
         int total = 0;
-        Game[] played = new FootballData().GetAllPlayed();
+        Game[] played = _footballData.GetAllPlayed();
         foreach (var game in played)
         {
             total = game.GetTeamScore(teamName, total);
