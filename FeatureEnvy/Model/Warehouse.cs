fd9958a640cc5cb63f1b16b3f8949e6e9e15a925
Lucas BK <lucas.blackaller-kidd@gmsl.co.uk>
2025-12-11 12:50:13 +0000

    something tock

---
+++ FeatureEnvy/Model/Warehouse.cs
@@ -0,0 +1,17 @@
+﻿namespace FeatureEnvy.Model;
+
+public class Warehouse
+{
+    public Warehouse(List<WarehouseStock> items)
+    {
+        Stocks = items;
+    }
+
+    public List<WarehouseStock> Stocks { get; }
+
+    public bool CheckStock(Product product, int qty)
+    {
+        var stock = this.Stocks.FirstOrDefault(s => s.Product == product);
+        return stock != null && stock.Quantity >= qty;
+    }
+}
--- FeatureEnvy/Services/StockService.cs
+++ FeatureEnvy/Services/StockService.cs
@@ -2,22 +2,6 @@
 
 namespace FeatureEnvy.Services;
 
-public class Warehouse
-{
-    public Warehouse(List<WarehouseStock> stocks)
-    {
-        Stocks = stocks;
-    }
-
-    public List<WarehouseStock> Stocks { get; }
-
-    public bool CheckStock(Product product, int qty)
-    {
-        var stock = this.Stocks.FirstOrDefault(s => s.Product == product);
-        return stock != null && stock.Quantity >= qty;
-    }
-}
-
 public class StockService
 {
     private readonly Warehouse _warehouse;
